a8458126f9018223d3984628f77c72bbfa27abe3
hare19012 <harisheric2015@gmail.com>
2023-06-06 22:28:47 +0200

    Zapakovao u docker, sve riješeno, privatni razgovor nije do kraja zavrsen

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var redis = require('redis');
-var  db = require("../konekcija.js");
+var db = require("../konekcija.js");
 var io;
-var redisClient = redis.createClient();
-/* GET home page. */
-let onlineKorisnici = {};
+var room = "123";
+
+// GET home page.
+let onlineUsers = {};
+
 router.get('/', function(req, res, next) {
+  // Generating a random name consisting of 8 characters, with the first letter capitalized
   var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var poruke = [];
   var name = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
@@ -15,88 +17,93 @@ router.get('/', function(req, res, next) {
     var randomLetter = alphabet.charAt(Math.floor(Math.random() * alphabet.length)).toLowerCase();
     name += randomLetter;
   }
+
   if (!io) {
     io = require('socket.io')(req.connection.server);
-    io.sockets.on('connection', function (client) {
-      client.on('Joino se klijent', function (ime) {
-        client.join("123");
-        db.pool.connect(function (err, client, done) {
+
+    io.sockets.on('connection', function(client) {
+      client.on('Client join', function(ime) {
+        // When a user joins the chat, add them to the room
+        client.join(room);
+
+        db.pool.connect(function(err, client, done) {
           if (err) {
             return res.send(err);
           }
-          client.query(`SELECT * FROM chat`, [], function (err, result) {
+          client.query(`SELECT * FROM chat`, [], function(err, result) {
             done();
             if (err) {
               return res.send(err);
             } else {
               poruke = result.rows;
               console.log("Emitting 'Sve_poruke' event to the client");
-              io.sockets.to("123").emit("Sve_poruke",poruke);
+              // Retrieve messages from the database and emit them to all users in the room
+              io.sockets.to(room).emit("All messages", poruke);
               console.log(poruke);
-
-
-
-
             }
-
           });
         });
-        onlineKorisnici[client.id] = ime; // Dodajte korisnika u objekat online korisnika
-        console.log(Object.values(onlineKorisnici));
-        io.sockets.to("123").emit('Online korisnici', Object.values(onlineKorisnici)); // Emitujte listu online korisnika svim klijentima u sobi
-        client.emit('USAO_U_RAZGOVOR', "You have joined the conversation!"); // Emitujte poruku samo klijentu koji se povezao
-        client.broadcast.to("123").emit('USAO_U_RAZGOVOR', ime); // Emitujte ime korisnika drugim klijentima u sobi
 
+        onlineUsers[client.id] = ime; // Add the user to the online users object
+        console.log(Object.values(onlineUsers));
+        io.sockets.to(room).emit('Online users', Object.values(onlineUsers)); // Emit the list of online users to all clients in the room
+        client.emit('Entered into the conversation', "You have joined the conversation!"); // Emit a message to the client who joined
+        client.broadcast.to(room).emit('Entered into the conversation', ime); // Emit the user's name to other clients in the room
 
-        client.on('disconnect', function () {
-          delete onlineKorisnici[client.id]; // Uklonite korisnika iz objekta online korisnika
-          client.broadcast.to("123").emit('Online korisnici', Object.values(onlineKorisnici)); // Emitujte ažuriranu listu online korisnika svim klijentima u sobi
-          client.broadcast.to("123").emit('IZASAO_IZ_RAZGOVORA', ime); // Emitujte događaj kada korisnik napusti razgovor
+        client.on('disconnect', function() {
+          delete onlineUsers[client.id]; // Remove the user from the online users object
+          client.broadcast.to(room).emit('Online users', Object.values(onlineUsers)); // Emit the updated list of online users to all clients in the room
+          client.broadcast.to(room).emit('Disconnected from the conversation', ime); // Emit an event when a user leaves the conversation
         });
       });
-      client.on('Salje poruku',function (poruka,ime){
-        db.pool.connect(function (err, client, done) {
+
+      client.on('Message', function(poruka, ime) {
+        db.pool.connect(function(err, client, done) {
           if (err) {
             return res.send(err);
           }
           client.query(`INSERT INTO chat(prouka,ime) 
-            VALUES ($1,$2)`, [poruka,ime], function (err, result) {
+            VALUES ($1,$2)`, [poruka, ime], function(err, result) {
             done();
             if (err) {
               return res.send(err);
             } else {
               var zadnja = [];
-              db.pool.connect(function (err, client, done) {
+              db.pool.connect(function(err, client, done) {
                 if (err) {
                   return res.send(err);
                 }
 
-
-                client.query(`select * from chat order by id desc limit 1`, [], function (err, result) {
+                client.query(`select * from chat order by id desc limit 1`, [], function(err, result) {
                   done();
                   if (err) {
                     return res.send(err);
-
                   } else {
                     zadnja = result.rows;
                     console.log(zadnja);
-                    io.sockets.in("123").emit("Svima poruka",zadnja);
-
+                    // Insert the message into the database, fetch it, and emit it to all users in the room
+                    io.sockets.in(room).emit("A message to everyone", zadnja);
                   }
-
                 });
               });
-
-
             }
-
           });
         });
       });
 
+      client.on("Private message", function(posiljalac, primaoc) {// private messages, on the frontend, I have an onclick function that sends the sender and recipient, I haven't finished this request yet
+        console.log(posiljalac + primaoc);
+        room = "private" + primaoc + posiljalac;
+        client.join(room);
+        client.emit('Entered into a private conversation', "You have joined a private conversation!", primaoc);
+        io.to(room).emit('Online private korisnici', Object.values(onlineUsers), primaoc);
+      });
     });
   }
-  res.render('index', { kod:'123',imee:name});
+
+  res.render('index', {
+    imee: name
+  });
 });
 
 module.exports = router;
